74e02edba0d59e2a4b5a640e04c30e83646d30fb
alienx5499 <p2005p5499p@gmail.com>
2026-02-22 21:03:48 +0530

    Ex3 Shipped

--- SOLID/Ex3/src/Main.java
+++ SOLID/Ex3/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Placement Eligibility ===");
         StudentProfile s = new StudentProfile("23BCS1001", "Ayaan", 8.10, 72, 18, LegacyFlags.NONE);
-        EligibilityEngine engine = new EligibilityEngine(new FakeEligibilityStore());
+        List<EligibilityRule> rules = new ArrayList<>();
+        rules.add(new DisciplinaryRule());
+        rules.add(new CgrRule());
+        rules.add(new AttendanceRule());
+        rules.add(new CreditsRule());
+        EligibilityEngine engine = new EligibilityEngine(new FakeEligibilityStore(), rules);
         engine.runAndPrint(s);
     }
 }
