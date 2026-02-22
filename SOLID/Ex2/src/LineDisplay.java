4b64f1aa879b405de19d9eae09175aeb81a2aa95
alienx5499 <p2005p5499p@gmail.com>
2026-02-22 18:58:32 +0530

    Ex2 Shipped

---
+++ SOLID/Ex2/src/LineDisplay.java
@@ -0,0 +1,11 @@
+public class LineDisplay {
+    public final String name;
+    public final int qty;
+    public final double lineTotal;
+
+    public LineDisplay(String name, int qty, double lineTotal) {
+        this.name = name;
+        this.qty = qty;
+        this.lineTotal = lineTotal;
+    }
+}
---
+++ SOLID/Ex2/src/OrderPricer.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class OrderPricer {
+    public PricerResult compute(Map<String, MenuItem> menu, List<OrderLine> orderLines) {
+        double sub = 0.0;
+
+        List<LineDisplay> list = new ArrayList<>();
+        for (OrderLine l : orderLines) {
+            MenuItem item = menu.get(l.itemId);
+            double lineTotal = item.price * l.qty;
+            sub += lineTotal;
+            list.add(new LineDisplay(item.name, l.qty, lineTotal));
+        }
+        
+        return new PricerResult(sub, list);
+    }
+}
