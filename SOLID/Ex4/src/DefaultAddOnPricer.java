f25cf85bb2a8b4ee554341cddde19f845caf8139
alienx5499 <p2005p5499p@gmail.com>
2026-02-22 22:32:09 +0530

    Ex4 Shipped

---
+++ SOLID/Ex4/src/DefaultAddOnPricer.java
@@ -0,0 +1,8 @@
+public class DefaultAddOnPricer implements AddOnPricer {
+    public double getPrice(AddOn a) {
+        if (a == AddOn.MESS) return 1000.0;
+        if (a == AddOn.LAUNDRY) return 500.0;
+        if (a == AddOn.GYM) return 300.0;
+        return 0.0;
+    }
+}
