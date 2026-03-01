358deda84acd18ac0c47127248d3eccc0391e176
alienx5499 <p2005p5499p@gmail.com>
2026-03-01 09:36:27 +0530

    Ex8 Shipped

--- SOLID/ex08/src/ClubAdminTools.java
+++ SOLID/ex08/src/ClubAdminTools.java
@@ -1,8 +1,17 @@
 public interface ClubAdminTools {
     // Fat interface (ISP violation)
+}
+
+interface FinanceTools extends ClubAdminTools {
     void addIncome(double amt, String note);
     void addExpense(double amt, String note);
+}
+
+interface MinutesTools extends ClubAdminTools {
     void addMinutes(String text);
+}
+
+interface EventTools extends ClubAdminTools {
     void createEvent(String name, double budget);
     int getEventsCount();
 }
