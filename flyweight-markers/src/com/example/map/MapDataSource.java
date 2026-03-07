7a3f3b0ac6c6906b2c3f3299f55b6aba4db501f2
alienx5499 <p2005p5499p@gmail.com>
2026-03-07 16:04:32 +0530

    Assignment 3 Shipped

--- flyweight-markers/src/com/example/map/MapDataSource.java
+++ flyweight-markers/src/com/example/map/MapDataSource.java
@@ -10,7 +10,7 @@ import java.util.Random;
  * CURRENT STATE (BROKEN ON PURPOSE):
  * - Creates new MarkerStyle per MapMarker via MapMarker constructor.
  *
- * TODO (student):
+ * DONE (student):
  * - After introducing MarkerStyleFactory, refactor so identical styles are shared.
  * - Suggested approach:
  *   1) Change MapMarker to accept MarkerStyle directly
@@ -21,6 +21,8 @@ public class MapDataSource {
     private static final String[] SHAPES = {"PIN", "CIRCLE", "SQUARE"};
     private static final String[] COLORS = {"RED", "BLUE", "GREEN", "ORANGE"};
     private static final int[] SIZES = {10, 12, 14, 16};
+
+    private final MarkerStyleFactory styleFactory = new MarkerStyleFactory();
 
     public List<MapMarker> loadMarkers(int count) {
         Random rnd = new Random(7);
@@ -37,7 +39,8 @@ public class MapDataSource {
             int size = SIZES[rnd.nextInt(SIZES.length)];
             boolean filled = rnd.nextBoolean();
 
-            out.add(new MapMarker(lat, lng, label, shape, color, size, filled));
+            MarkerStyle style = styleFactory.get(shape, color, size, filled);
+            out.add(new MapMarker(lat, lng, label, style));
         }
         return out;
     }
